2dca1d97e615bf289db04ba42c46bd2b1277b49a
enr1c091 <enrico_bergamo_91@hotmail.com>
2020-05-24 14:08:07 -0300

    (logging) Parse X-Ray Tracing IDs

--- backend/src/lib/logging/logger.js
+++ backend/src/lib/logging/logger.js
@@ -3,7 +3,7 @@ const AWS = AWSXRay.captureAWS(require('aws-sdk'))
 const cloudwatch = new AWS.CloudWatch()
 const log = require('lambda-log')
 const MetricUnit = require('../helper/models')
-
+const TRACE_ID_REGEX = /^Root=(.+);Parent=(.+);/;
 /**
  * Prepares logger class to be used across methods.
  * 
@@ -15,11 +15,16 @@ const MetricUnit = require('../helper/models')
  * @returns     {LambdaLog} Logger class.
  */
 exports.logger_setup = () => {
+    const tracingInfo = process.env._X_AMZN_TRACE_ID || '';
+    const matches = tracingInfo.match(TRACE_ID_REGEX) || ['', '', ''];
+
     log.options.debug = process.env.ENABLE_DEBUG !== undefined ? process.env.ENABLE_DEBUG : false
     log.options.dynamicMeta = message => {
         return {
             timestamp: new Date().toISOString(),
-            'X-Amzn-Trace-Id': process.env._X_AMZN_TRACE_ID
+            'X-Amzn-Trace-Id': process.env._X_AMZN_TRACE_ID,
+            'X-Amzn-Trace-Id-Root': matches[1],
+            'X-Amzn-Trace-Id-Parent': matches[2]
         }
     }
     return log
