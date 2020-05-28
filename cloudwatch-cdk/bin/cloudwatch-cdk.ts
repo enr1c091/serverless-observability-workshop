66645e0da783e7fb29fe616d8a09f9539c8463fa
enr1c091 <enrico_bergamo_91@hotmail.com>
2020-05-27 21:41:46 -0300

    (Monitoring)Fixed CDK project for CloudWatch Dashboards

--- cloudwatch-cdk/bin/cloudwatch-cdk.ts
+++ cloudwatch-cdk/bin/cloudwatch-cdk.ts
@@ -2,6 +2,19 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CloudwatchCdkStack } from '../lib/cloudwatch-cdk-stack';
+import { CloudFormationHelper } from '../lib/helper/cloudformation-parser';
 
-const app = new cdk.App();
-new CloudwatchCdkStack(app, 'CloudwatchCdkStack');
+(async () => {
+    const app = new cdk.App();
+    const monitoringStackName = app.node.tryGetContext('stack_name')
+    if (!monitoringStackName) {
+        console.log("****************************************************");
+        console.log("ERROR: your AWS SAM backend CloudFormation stack_name is undefined.\n");
+        console.log("Please run the command like this:");
+        console.log("cdk [synth|deploy|destroy] -c stack_name=<your SAM stack name>");
+        console.log("****************************************************");
+        process.exit(1);
+    }
+    const functions = await new CloudFormationHelper().cfn_parser(monitoringStackName)
+    new CloudwatchCdkStack(app, 'CloudwatchCdkStack', { functions: functions });
+})()
--- cloudwatch-cdk/lib/cloudwatch-cdk-stack.ts
+++ cloudwatch-cdk/lib/cloudwatch-cdk-stack.ts
@@ -1,36 +1,42 @@
 import * as cdk from '@aws-cdk/core'
 import cloudwatch = require('@aws-cdk/aws-cloudwatch')
-import { GraphWidget, SingleValueWidget, Metric, DimensionHash } from "@aws-cdk/aws-cloudwatch"
+import { GraphWidget, SingleValueWidget, Metric, DimensionHash } from '@aws-cdk/aws-cloudwatch'
+import { CloudFormationHelper } from './helper/cloudformation-parser'
+
+export interface MyStackProps extends cdk.StackProps {
+  functions?: any;
+}
 
 export class CloudwatchCdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props: MyStackProps = {}) {
     super(scope, id, props);
 
-    const stackName = this.node.tryGetContext('stack_name')
-    const opDashboard = new cloudwatch.Dashboard(this, 'MonitoringApp-Operational-Dashboard', { dashboardName: 'MonitoringApp-Operational-Dashboard' })
-    opDashboard.addWidgets(
+    const operationalDashboard = new cloudwatch.Dashboard(this, 'MonitoringApp-Operational-Dashboard', { dashboardName: 'MonitoringApp-Operational-Dashboard' })
+    operationalDashboard.addWidgets(
       this.buildSingleValueWidget('Get All Items -- Operational Metrics', 'monitoring-getAllItemsFunction-1OFYBMQ0CWM1D'),
       this.buildSingleValueWidget('Get Item By ID -- Operational Metrics', 'monitoring-getByIdFunction-PJDC795HE5YP'),
       this.buildSingleValueWidget('Put Item -- Operational Metrics', 'monitoring-putItemFunction-U6EIYZDPS5B5'),
     );
 
-    const bdDashboard = new cloudwatch.Dashboard(this, 'MonitoringApp-Business-Dashboard', { dashboardName: 'MonitoringApp-Business-Dashboard' })
-    bdDashboard.addWidgets(
-      this.buildGraphWidget('Business Metrics')
+    const businessDashboard = new cloudwatch.Dashboard(this, 'MonitoringApp-Business-Dashboard', { dashboardName: 'MonitoringApp-Business-Dashboard' })
+    businessDashboard.addWidgets(
+      this.buildGraphWidget('Item Service - Business Metrics', props.functions)
     );
   }
 
   // Example with Graph Widget
-  buildGraphWidget(widgetName: string, functionName: string = ''): GraphWidget {
+  buildGraphWidget(widgetName: string, functions: any = {}): GraphWidget {
     return new GraphWidget({
       title: widgetName,
+      height: 6,
+      width: 24,
       left: [
-        this.buildMetric('MonitoringApp', 'Sucessful Item Registrations', 'monitoring-putItemFunction-U6EIYZDPS5B5', 'SuccessfullPutItem', 'sum', cdk.Duration.hours(1), { FunctionName: 'monitoring-putItemFunction-U6EIYZDPS5B5', FunctionVersion: '$LATEST', service: 'item_service', operation: 'put-item' }),
-        this.buildMetric('MonitoringApp', 'Failed Item Registrations', 'monitoring-putItemFunction-U6EIYZDPS5B5', 'FailedPutItem', 'sum', cdk.Duration.hours(1), { FunctionName: 'monitoring-putItemFunction-U6EIYZDPS5B5', FunctionVersion: '$LATEST', service: 'item_service', operation: 'put-item' }),
-        this.buildMetric('MonitoringApp', 'Sucessful Items Retrievals', 'monitoring-getByIdFunction-PJDC795HE5YP', 'SuccessfullGetItem', 'sum', cdk.Duration.hours(1), { FunctionName: 'monitoring-getByIdFunction-PJDC795HE5YP', FunctionVersion: '$LATEST', service: 'item_service', operation: 'get-by-id' }),
-        this.buildMetric('MonitoringApp', 'Failed Items Retrievals', 'monitoring-getByIdFunction-PJDC795HE5YP', 'FailedGetItem', 'sum', cdk.Duration.hours(1), { FunctionName: 'monitoring-getByIdFunction-PJDC795HE5YP', FunctionVersion: '$LATEST', service: 'item_service', operation: 'get-by-id' }),
-        this.buildMetric('MonitoringApp', 'Sucessful Items Retrievals', 'monitoring-getAllItemsFunction-1OFYBMQ0CWM1D', 'SuccessfullGetAllItems', 'sum', cdk.Duration.hours(1), { FunctionName: 'monitoring-getAllItemsFunction-1OFYBMQ0CWM1D', FunctionVersion: '$LATEST', service: 'item_service', operation: 'get-all-items' }),
-        this.buildMetric('MonitoringApp', 'Failed Items Retrievals', 'monitoring-getAllItemsFunction-1OFYBMQ0CWM1D', 'FailedGetAllItems', 'sum', cdk.Duration.hours(1), { FunctionName: 'monitoring-getAllItemsFunction-1OFYBMQ0CWM1D', FunctionVersion: '$LATEST', service: 'item_service', operation: 'get-all-items' })
+        this.buildMetric('MonitoringApp', 'Sucessful Item Registrations', functions.putItemFunction, 'SuccessfullPutItem', 'sum', cdk.Duration.minutes(1), { FunctionName: functions.putItemFunction, FunctionVersion: '$LATEST', service: 'item_service', operation: 'put-item' }),
+        this.buildMetric('MonitoringApp', 'Failed Item Registrations', functions.putItemFunction, 'FailedPutItem', 'sum', cdk.Duration.minutes(1), { FunctionName: functions.putItemFunction, FunctionVersion: '$LATEST', service: 'item_service', operation: 'put-item' }),
+        this.buildMetric('MonitoringApp', 'Sucessful ItemByID Retrievals', functions.getByIdFunction, 'SuccessfullGetItem', 'sum', cdk.Duration.minutes(1), { FunctionName: functions.getByIdFunction, FunctionVersion: '$LATEST', service: 'item_service', operation: 'get-by-id' }),
+        this.buildMetric('MonitoringApp', 'Failed ItemByID Retrievals', functions.getByIdFunction, 'FailedGetItem', 'sum', cdk.Duration.minutes(1), { FunctionName: functions.getByIdFunction, FunctionVersion: '$LATEST', service: 'item_service', operation: 'get-by-id' }),
+        this.buildMetric('MonitoringApp', 'Sucessful Items Retrievals', functions.getAllItemsFunction, 'SuccessfullGetAllItems', 'sum', cdk.Duration.minutes(1), { FunctionName: functions.getAllItemsFunction, FunctionVersion: '$LATEST', service: 'item_service', operation: 'get-all-items' }),
+        this.buildMetric('MonitoringApp', 'Failed Items Retrievals', functions.getAllItemsFunction, 'FailedGetAllItems', 'sum', cdk.Duration.minutes(1), { FunctionName: functions.getAllItemsFunction, FunctionVersion: '$LATEST', service: 'item_service', operation: 'get-all-items' })
       ]
     })
   }
@@ -39,13 +45,15 @@ export class CloudwatchCdkStack extends cdk.Stack {
   buildSingleValueWidget(widgetName: string, functionName: string = ''): SingleValueWidget {
     return new SingleValueWidget({
       title: widgetName,
+      height: 6,
+      width: 24,
       metrics: [
-        this.buildMetric('MonitoringApp', '# of GetItems ColdStarts', functionName, 'ColdStart', 'sum', cdk.Duration.hours(1), { FunctionName: functionName, FunctionVersion: '$LATEST', service: 'item_service', function_name: functionName }),
-        this.buildMetric('AWS/Lambda', 'GetItems ColdStart Duration', functionName, 'InitDuration', 'avg', cdk.Duration.hours(1), { FunctionName: functionName, FunctionVersion: '$LATEST' }),
-        this.buildMetric('AWS/Lambda', 'GetItems Billed Duration', functionName, 'BilledDuration', 'avg', cdk.Duration.hours(1), { FunctionName: functionName, FunctionVersion: '$LATEST' }),
-        this.buildMetric('AWS/Lambda', 'GetItems Billed Duration', functionName, 'MemorySize', 'avg', cdk.Duration.hours(1), { FunctionName: functionName, FunctionVersion: '$LATEST' }),
-        this.buildMetric('AWS/Lambda', 'GetItems Billed Duration', functionName, 'MemoryUsed', 'avg', cdk.Duration.hours(1), { FunctionName: functionName, FunctionVersion: '$LATEST' }),
-        this.buildMetric('AWS/Lambda', 'GetItems Estimated Cost ($)', functionName, 'EstimatedCost', 'sum', cdk.Duration.hours(1), { FunctionName: functionName, FunctionVersion: '$LATEST' })
+        this.buildMetric('MonitoringApp', '# of ColdStarts', functionName, 'ColdStart', 'sum', cdk.Duration.hours(1), { FunctionName: functionName, FunctionVersion: '$LATEST', service: 'item_service', function_name: functionName }),
+        this.buildMetric('AWS/Lambda', 'ColdStart Duration', functionName, 'InitDuration', 'avg', cdk.Duration.hours(1), { FunctionName: functionName, FunctionVersion: '$LATEST' }),
+        this.buildMetric('AWS/Lambda', 'Billed Duration', functionName, 'BilledDuration', 'avg', cdk.Duration.hours(1), { FunctionName: functionName, FunctionVersion: '$LATEST' }),
+        this.buildMetric('AWS/Lambda', 'Memory Size', functionName, 'MemorySize', 'avg', cdk.Duration.hours(1), { FunctionName: functionName, FunctionVersion: '$LATEST' }),
+        this.buildMetric('AWS/Lambda', 'Memory Used', functionName, 'MemoryUsed', 'avg', cdk.Duration.hours(1), { FunctionName: functionName, FunctionVersion: '$LATEST' }),
+        this.buildMetric('AWS/Lambda', 'Estimated Cost ($)', functionName, 'EstimatedCost', 'sum', cdk.Duration.hours(1), { FunctionName: functionName, FunctionVersion: '$LATEST' })
       ]
     })
   }
